c32a9a0c06a5f752e733248a2b1e9be23fef9ac7
Allen <guolong613@gmail.com>
2022-12-13 06:27:07 +0000

    完成四个页面

--- src/pages/Welcome3.tsx
+++ src/pages/Welcome3.tsx
@@ -1,5 +1,13 @@
+import p from '../assets/images/welcome3.svg'
+
 export const Welcome3: React.FC = () => {
   return (
-    <div> 3 </div>
+    <div text-center>
+      <img src={p} />
+      <h2 text-32px mt-48px >
+        数据可视化 <br />
+        收支一目了然
+      </h2>
+    </div>
   )
 }
--- src/pages/Welcome4.tsx
+++ src/pages/Welcome4.tsx
@@ -1,5 +1,13 @@
+import p from '../assets/images/welcome4.svg'
+
 export const Welcome4: React.FC = () => {
   return (
-    <div> 4 </div>
+    <div text-center>
+      <img src={p} />
+      <h2 text-32px mt-48px >
+        云备份 <br />
+        再也不怕数据丢失
+      </h2>
+    </div>
   )
 }
