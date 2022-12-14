5b95e43dc0d5f23838687b77ab292001e6824997
Allen <guolong613@gmail.com>
2022-12-14 08:21:34 +0000

    完成首页样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,18 @@
-import useSWR from 'swr'
-import axios from 'axios'
-const fetcher = (path: string) => {
-  return axios.get(path).then(res => res.data)
-}
-
+import p from '../assets/images/pig.svg'
+import add from '../assets/icons/add.svg'
 export const Home: React.FC = () => {
-  const { data, error, isValidating, mutate } = useSWR('/api/xxx', fetcher)
-  const isLoading = !data && !error
-  const onClick = async () => {
-    const data = await axios.post('xxx')
-    mutate(data)
-  }
-  if (error)
-    return <div>failed to load</div>
-  if (isLoading)
-    return <div>loading</div>
-  if (isValidating)
-    return <div>正在获取最新值</div>
-
-  return (
-    <div>{data?.data.message}</div>
-  )
+  return <div>
+    <div flex justify-center items-center>
+      <img mt-20vh mb-20vh width="128" height="130" src={p} />
+    </div>
+    <div px-16px>
+      <button h-48px w="100%" bg="#5C33BE" b-none text-white
+        rounded-8px
+      >开始记账</button>
+    </div>
+    <button p-4px w-56px h-56px bg="#5C33BE" rounded="50%" b-none text-white
+      text-6xl fixed bottom-16px right-16px>
+      <img src={add} max-w="100%" max-h="100%" />
+    </button>
+  </div>
 }
