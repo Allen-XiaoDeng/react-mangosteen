ca1e94252f5f1da84e792aa5e56cd27631672e35
Allen <guolong613@gmail.com>
2022-12-15 07:20:39 +0000

    完成时间范围选择组件 TimeRangePicker

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,20 +1,22 @@
 import styled from 'styled-components'
+import { useState } from 'react'
 import { AddItemFloatButton } from '../components/AddItemFloatButton'
 import { TimeRangePicker } from '../components/TimeRangePicker'
 import { Topnav } from '../components/Topnav'
+import type { TimeRange } from '../components/TimeRangePicker'
 import { ItemsList } from './ItemsPage/ItemsList'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
-
 const Div = styled.div`
   background: linear-gradient(0deg, rgba(143,76,215,1) 0%, rgba(92,51,190,1) 100%);
 `
 
 export const ItemsPage: React.FC = () => {
+  const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
   return (
     <div>
       <Div>
         <Topnav />
-        <TimeRangePicker />
+        <TimeRangePicker selected={timeRange} onSelected={setTimeRange}/>
       </Div>
       <ItemsSummary />
       <ItemsList />
