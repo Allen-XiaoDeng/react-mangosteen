5f66d105ff74b3a4c0a723fb376c70628b48bdfb
Allen <guolong613@gmail.com>
2022-12-15 03:16:03 +0000

    使用 import 确定 css 顺序

--- uno.config.ts
+++ uno.config.ts
@@ -9,18 +9,18 @@ export default defineConfig({
   shortcuts: {
   },
   safelist: [],
-  preflights: [
-    {
-      layer: 'components',
-      getCSS: () => `
-        .j-icon{
-          fill: currentColor;
-          width: 1.2em;
-          height: 1.2em;
-        }
-      `
-    },
-  ],
+  // preflights: [
+  //   {
+  //     layer: 'components',
+  //     getCSS: () => `
+  //       .j-icon{
+  //         fill: currentColor;
+  //         width: 1.2em;
+  //         height: 1.2em;
+  //       }
+  //     `
+  //   },
+  // ],
   presets: [
     presetUno(),
     presetAttributify(),
