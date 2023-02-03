c3c30d0a5518cce756033e98b53a71ae0881724d
Allen <guolong613@gmail.com>
2023-02-03 09:46:40 +0000

    完成记账页面的顶部导航栏

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
+import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
 
+type ItemKind = 'income' | 'expense'
 export const ItemsNewPage: React.FC = () => {
+  const tabItems: { key: ItemKind; text: string }[]
+    = [{ key: 'income', text: '支出' }, { key: 'expense', text: '收入' }]
+  const [tabItem, setTabItem] = useState<ItemKind>('income')
+
   return (
     <div>
       <Gradient>
-        <TopNav title="记一笔" icon={<Icon name="back" />}/>
+        <TopNav title="记一笔" icon={<Icon name="back" />} />
+        <Tabs tabItems={tabItems}
+          className="children-flex-1 text-center"
+          value={tabItem} onChange={(item) => { setTabItem(item) }}
+        />
       </Gradient>
     </div>
   )
