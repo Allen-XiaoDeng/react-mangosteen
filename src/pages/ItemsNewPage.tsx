f2e2cc5b5170b7b54f32fb9549f18ad934629a41
Allen <guolong613@gmail.com>
2023-02-06 08:42:03 +0000

    使用 Grid 进行 Tags 的布局

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -4,27 +4,27 @@ import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import { Tags } from './ItemsNewPage/Tags'
 
-type ItemKind = 'income' | 'expense'
 export const ItemsNewPage: React.FC = () => {
-  const tabItems: { key: ItemKind; text: string; element?: ReactNode }[]
+  const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[]
     = [
-      { key: 'expense', text: '支出', element: <div>支出</div> },
-      { key: 'income', text: '收入', element: <div>收入</div> }
+      { key: 'expenses', text: '支出', element: <Tags kind="expenses" /> },
+      { key: 'income', text: '收入', element: <Tags kind="income" /> }
     ]
-  const [tabItem, setTabItem] = useState<ItemKind>('income')
+  const [tabItem, setTabItem] = useState<Item['kind']>('income')
 
   return (
     <div>
       <Gradient>
         <TopNav title="记一笔" icon={<Icon name="back" />} />
-        </Gradient>
-        <Tabs
-          classPrefix='itemsNewPage'
-          tabItems={tabItems}
-          className="text-center"
-          value={tabItem} onChange={(item) => { setTabItem(item) }}
-        />
+      </Gradient>
+      <Tabs
+        classPrefix='itemsNewPage'
+        tabItems={tabItems}
+        className="text-center"
+        value={tabItem} onChange={(item) => { setTabItem(item) }}
+      />
     </div>
   )
 }
