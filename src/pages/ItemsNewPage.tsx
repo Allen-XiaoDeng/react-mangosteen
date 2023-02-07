e7713bebe50ae35fd4c492aad5c567ae6d4ce57d
Allen <guolong613@gmail.com>
2023-02-07 01:30:03 +0000

    基本完成 ItemsNewPage 的布局

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -4,7 +4,9 @@ import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import { DateAndAmount } from './ItemsNewPage/DateAndAmount'
 import { Tags } from './ItemsNewPage/Tags'
+import s from './ItemsNewPage.module.scss'
 
 export const ItemsNewPage: React.FC = () => {
   const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[]
@@ -15,16 +17,17 @@ export const ItemsNewPage: React.FC = () => {
   const [tabItem, setTabItem] = useState<Item['kind']>('income')
 
   return (
-    <div>
-      <Gradient>
+    <div className={s.wrapper} h-screen flex flex-col>
+      <Gradient className="grow-0 shrink-0">
         <TopNav title="记一笔" icon={<Icon name="back" />} />
       </Gradient>
       <Tabs
         classPrefix='itemsNewPage'
         tabItems={tabItems}
-        className="text-center"
+        className="text-center grow-1 shrink-1 overflow-hidden"
         value={tabItem} onChange={(item) => { setTabItem(item) }}
       />
+      <DateAndAmount className="grow-0 shrink-0" />
     </div>
   )
 }
