d920b2fb150b0f9c57ce29b3a576a6cb53b95788
Allen <guolong613@gmail.com>
2023-02-14 05:39:29 +0000

    创建 LineChart 组件

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,40 @@
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
+type Props = {
+  className?: string
+  items?: { x: number | string; y: number }[]
+}
+
+export const LineChart: React.FC<Props> = (props) => {
+  const { className, items } = props
+  const xItems = items?.map(item => item.x)
+  const yItems = items?.map(item => item.y)
+  const div = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (!div.current) {
+      return
+    }
+    const myChart = echarts.init(div.current)
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: xItems,
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: yItems,
+          type: 'line'
+        }
+      ]
+    }
+    myChart.setOption(option)
+  }, [])
+
+  return (
+    <div ref={div} className={className}></div>
+  )
+}
