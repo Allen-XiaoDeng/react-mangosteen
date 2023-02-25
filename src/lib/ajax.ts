93748f6ddb943cee9d625b5d573c60e3817b11c1
Allengl <guolong613@gmail.com>
2023-02-25 06:48:09 +0000

    Ajax 统一错误处理

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -6,8 +6,9 @@
 // 3. 函数的返回值尽量少，最好是一个
 // 4. 函数的副作用尽量少，最好是没有
 
-import type { AxiosRequestConfig } from 'axios'
+import type { AxiosError, AxiosRequestConfig } from 'axios'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { useLoadingStore } from '../stores/useLoadingStore'
 
 // 静态配置项直接用 defaults 配置
@@ -26,17 +27,45 @@ axios.interceptors.request.use((config) => {
 // 封装 axios
 type Options = {
   showLoading?: boolean
+  handleError?: boolean
 }
 export const useAjax = (options?: Options) => {
+  const table: Record<string, undefined | (() => void)> = {
+    401: () => {
+      nav('/sign_in')
+    },
+    402: () => {
+      window.alert('请付费后观看')
+    },
+    403: () => {
+      window.alert('没有权限')
+    },
+    unknown: () => {
+      window.alert('未知错误')
+    }
+  }
+
   const showLoading = options?.showLoading || false
+  const handleError = options?.handleError ?? true
   const { setVisible } = useLoadingStore()
+  const nav = useNavigate()
+  const onError = (error: AxiosError) => {
+    if (error.response) {
+      if (handleError) {
+        const { status } = error.response
+        const fn = table[status] || table.unknown
+        fn?.()
+      }
+    }
+    throw error
+  }
   const ajax = {
     get: <T>(path: string, config?: AxiosRequestConfig<any>) => {
-      return axios.get<T>(path, config)
+      return axios.get<T>(path, config).catch(onError)
     },
     post: <T>(path: string, data: JSONValue) => {
       if (showLoading) { setVisible(true) }
-      return axios.post<T>(path, data).finally(() => {
+      return axios.post<T>(path, data).catch(onError).finally(() => {
         if (showLoading) { setVisible(false) }
       })
     },
