dda590cb20061da68ab5a70326a40914cec1a355
Allengl <guolong613@gmail.com>
2023-03-24 14:38:44 +0000

    feat: 通过 /items/balance 获取收支信息

--- mock/items.mock.ts
+++ mock/items.mock.ts
@@ -1,12 +1,10 @@
 import type { MockMethod } from 'vite-plugin-mock'
 import { faker } from '@faker-js/faker'
-
 let id = 0
 const createId = () => {
   id += 1
   return id
 }
-
 const create = (attrs?: Partial<Item>): Item => {
   return {
     id: createId(),
@@ -20,12 +18,10 @@ const create = (attrs?: Partial<Item>): Item => {
     ...attrs
   }
 }
-
 const createList = (n: number, attrs?: Partial<Item>): Item[] => {
-  return Array.from({ length: n }).map(() => create())
+  return Array.from({ length: n }).map(() => create(attrs))
 }
-
-const createResponse = ({ count = 10, perPage = 10, page = 1 }, attrs?: Partial<Item>): Resources<Item> => {
+const createResponse = ({ count = 10, perPage = 10, page = 1 }, attrs?: Partial<Item>,): Resources<Item> => {
   const sendCount = (page - 1) * perPage
   const left = count - sendCount
   return {
@@ -37,13 +33,20 @@ const createResponse = ({ count = 10, perPage = 10, page = 1 }, attrs?: Partial<
     }
   }
 }
-
 export const itemsMock: MockMethod[] = [{
   url: '/api/v1/items',
   method: 'get',
   statusCode: 200,
-  timeout: 500,
-  response: ({ query }: ResponseParams): Resources<Item> => {
-    return createResponse({ count: 90, perPage: 10, page: parseInt(query.page) })
-  }
+  response: ({ query }: ResponseParams): Resources<Item> =>
+    createResponse({ count: 90, perPage: 10, page: parseInt(query.page) || 1 })
+  ,
+}, {
+  url: '/api/v1/items/balance',
+  method: 'get',
+  statusCode: 200,
+  response: () => ({
+    balance: 40400,
+    expenses: 90900,
+    income: 131300
+  })
 }]
